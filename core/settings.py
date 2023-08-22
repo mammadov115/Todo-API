c61b815b4994ba8b6e8738d6741d6893f28fcd3a
mammadov115 <mehmanmehman182@gmail.com>
2023-08-22 13:59:20 +0400

    drf

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo.apps.TodoConfig'
+    # 3rd party
+    'rest_framework',
+    # local
+    'todo.apps.TodoConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -122,3 +126,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
